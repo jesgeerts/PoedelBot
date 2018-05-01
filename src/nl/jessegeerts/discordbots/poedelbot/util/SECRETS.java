fa52b65af3d13b89395b02fb9a766836b8552cb1
Jesse Geerts <me@jessegeerts.nl>
2018-05-01 20:22:00 +0200

    Update SECRETS.java

--- src/nl/jessegeerts/discordbots/poedelbot/util/SECRETS.java
+++ src/nl/jessegeerts/discordbots/poedelbot/util/SECRETS.java
@@ -2,5 +2,5 @@ package nl.jessegeerts.discordbots.poedelbot.util;
 
 public class SECRETS {
     public static String TOKEN =
-            "NDE3MzI2MjQxOTgxNjYxMTk0.DXRY3A.PtdfBP3Q4WMVakgCtJ2fHMVztjQ";
+            "YOUR_BOT_TOKEN";
 }
